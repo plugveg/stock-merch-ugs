b45681ca04829a0374682c7dc49c4e667e35cccb
setsuma <plug.veg@gmail.com>
2025-04-08 16:20:47 +0200

    ci: update commitlint config

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,8 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  ignores: [(message) => message.startsWith("chore(release):")],
+  rules: {
+    "body-max-line-length": [1, "always", 100],
+    "footer-max-line-length": [1, "always", 100],
+  },
+};
