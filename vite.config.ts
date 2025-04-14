e5d77a2cbd8106f38e92adcfc524d34d0a4675cf
setsuma <plug.veg@gmail.com>
2025-04-14 09:35:39 +0200

    fix: tailwind with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { codecovVitePlugin } from "@codecov/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     codecovVitePlugin({
       enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
       bundleName: "StockMerchUGS",
